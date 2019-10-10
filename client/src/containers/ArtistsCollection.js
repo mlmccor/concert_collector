14fb31f3f4830f290d05560b1e3b80e6e1f8574a
Matthew McCormack <mmccormack1182@gmail.com>
2019-10-10 14:09:22 -0400

    began adding images to ArtistList

--- client/src/containers/ArtistsCollection.js
+++ client/src/containers/ArtistsCollection.js
@@ -4,7 +4,12 @@ const ArtistsCollection = ({artistList, selectArtist}) => {
   return (
     <div className= 'container border border-dark rounded-pill' id='artistCollection container'>
       <h4>My Artists</h4>
-      {artistList.map((artist) => <><p id={artist.id} onClick={(e) => selectArtist(e.target.id)}>{artist.name}</p></>)}
+      {artistList.map((artist) => {
+        return (
+          <img src={artist.image_url} id={artist.id} onClick={(e) => selectArtist(e.target.id)}/>
+        )
+      })}
+
     </div>
   )
 }
